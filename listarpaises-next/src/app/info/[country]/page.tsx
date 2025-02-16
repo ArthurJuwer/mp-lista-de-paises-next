936d773365c24174dfafd617669689543222cd50
ArthurJuwer <arthur.juwer99@gmail.com>
2025-02-16 16:46:19 -0300

    correçao

--- listarpaises-next/src/app/info/[country]/page.tsx
+++ listarpaises-next/src/app/info/[country]/page.tsx
@@ -1,21 +1,8 @@
 import ListarItemApi from "@/components/ListarItemApi";
-import { GetServerSideProps } from "next";
-
 interface CountryPageProps {
   params: { country: string };
 }
 
 export default function Country({ params }: CountryPageProps) {
-  return <ListarItemApi key="item01" name={params.country} />;
+  return <ListarItemApi key="item01" name={params?.country} />;
 }
-
-// Se você estiver usando SSR, adicione esta função
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  const { country } = context.params as { country: string };
-
-  return {
-    props: {
-      params: { country },
-    },
-  };
-};

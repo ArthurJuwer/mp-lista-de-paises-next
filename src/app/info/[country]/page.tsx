7163537ce1056b0735f0f76780dd84e3527dd0a6
ArthurJuwer <arthur.juwer99@gmail.com>
2025-02-28 22:14:33 -0300

    novo ajuste

--- src/app/info/[country]/page.tsx
+++ src/app/info/[country]/page.tsx
@@ -1,8 +1,12 @@
+
 import ListarItemApi from "@/components/ListarItemApi";
 // interface CountryPageProps {
 //   params: { country: string };
 // }
 
-export default function Country({ params }: {params: { country: string}}) {
-  return <ListarItemApi key="item01" name={params?.country} />;
+export default function Country({ params }: { params: { country: string } }) {
+  const { country } = params;  // Destructure params correctly here
+  return <ListarItemApi key="item01" name={country} />;
 }
+
+
